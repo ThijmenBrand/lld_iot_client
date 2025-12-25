c6424bb0ebf0c2d52816358a0219966d37bda8c0
Thijmen Brand <Thijmen@ik.nu>
2025-12-25 20:27:53 +0100

    update cal

--- src/app/api/iot/widgets/calendar.ts
+++ src/app/api/iot/widgets/calendar.ts
@@ -1,6 +1,6 @@
 import { User } from "@/src/types/User";
 import { getOAuth } from "@/src/utils/auth";
-import { google } from "googleapis";
+import { calendar_v3, google } from "googleapis";
 
 export async function getCalendarData(user: User) {
   const oAuth = await getOAuth(user.id);
@@ -16,34 +16,60 @@ export async function getCalendarData(user: User) {
     const response = await calendar.events.list({
       calendarId: calendarId,
       timeMin: new Date().toISOString(),
-      maxResults: 3,
+      maxResults: 4,
       singleEvents: true,
       orderBy: "startTime",
     });
 
-    const events = response.data.items || [];
-    const lines = events.map((event) => {
-      const start = event.start?.dateTime || event.start?.date || "";
-      const summary = event.summary || "No Title";
-
-      const dateObj = new Date(start);
-
-      const timeStr = dateObj.toLocaleDateString("nl-NL", {
-        day: "numeric",
-        month: "short",
-        timeZone: "Europe/Amsterdam",
-      });
-
-      return `${timeStr} ${summary}`;
-    });
-
-    if (lines.length === 0) {
-      lines.push("No upcoming events.");
+    const items = response.data.items || [];
+    if (items.length === 0) {
+      return "No events.";
     }
 
-    return lines.join("|");
+    const first = items[0];
+    const highlight = {
+      time: formatTimeRange(first),
+      title: (first.summary || "No Title").substring(0, 25),
+    };
+
+    const upcoming = items.slice(1).map((event) => ({
+      time: formatUpcomingTime(event),
+      title: (event.summary || "No Title").substring(0, 25),
+    }));
+
+    return {
+      highlight,
+      upcoming,
+    };
   } catch (error) {
     console.error("Error fetching calendar data:", error);
     throw error;
   }
+}
+
+// --- Formatting Helpers ---
+function formatTimeRange(event: calendar_v3.Schema$Event) {
+  const start = new Date(event.start?.dateTime || event.start?.date || "");
+  const end = new Date(event.end?.dateTime || event.end?.date || "");
+
+  return `${start.getHours()}:${start
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")} - ${end.getHours()}:${end
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}`;
+}
+
+function formatUpcomingTime(event: calendar_v3.Schema$Event) {
+  const start = new Date(event.start?.dateTime || event.start?.date || "");
+  const month = start.toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+  });
+  const time = `${start.getHours()}:${start
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}`;
+  return `${month} ${time}`;
 }
